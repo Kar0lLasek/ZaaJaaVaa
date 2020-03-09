83adb8adcd830ef3b6637be234d968a380eda5c0
goloneczka <48791689+goloneczka@users.noreply.github.com>
2020-03-09 15:26:02 +0100

    settings for project

--- VIEW/src/main/java/ModelId/App.java
+++ VIEW/src/main/java/ModelId/App.java
@@ -1,5 +1,7 @@
 package ModelId;
 
+import ZaaJaaVaa.Stary;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Stary stary = new Stary();
     }
 }
