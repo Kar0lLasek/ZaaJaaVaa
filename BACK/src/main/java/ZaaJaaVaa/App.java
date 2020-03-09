9cc7b684ff4f02e638398a23ca3739134cb62cea
Bonniu <radekg23@o2.pl>
2020-03-09 15:48:33 +0100

    refactor

--- BACK/src/main/java/ZaaJaaVaa/App.java
+++ BACK/src/main/java/ZaaJaaVaa/App.java
@@ -1,14 +1,11 @@
 package ZaaJaaVaa;
 
 /**
- * Hello world!
- *
+ * BACK
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
         Stary s = new Stary();
         System.out.println(s.getAge());
     }
--- BACK/src/main/java/ZaaJaaVaa/Stary.java
+++ BACK/src/main/java/ZaaJaaVaa/Stary.java
@@ -5,18 +5,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class Stary {
-    @Getter @Setter private int age = 10;
+    @Getter
+    @Setter
+    private int age = 10;
 
-    /**
-     * Name of the person.
-     * -- SETTER --
-     * Changes the name of this person.
-     *
-     * @param name The new value.
-     */
-    @Setter(AccessLevel.PROTECTED) private String name;
+    @Setter(AccessLevel.PROTECTED)
+    private String name;
 
-    @Override public String toString() {
+    @Override
+    public String toString() {
         return String.format("%s (age: %d)", name, age);
     }
 }
