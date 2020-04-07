dc81c5579d8fcaa12c30fa9ec72323e5e9fd4789
goloneczka <48791689+goloneczka@users.noreply.github.com>
2020-04-07 13:34:54 +0200

    init Spring with javaFx (#7)

---
+++ JavaProject/app/src/main/java/app/hotel/App.java
@@ -0,0 +1,13 @@
+package app.hotel;
+
+
+
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
---
+++ JavaProject/app/src/main/java/app/hotel/controllers/ChartController.java
@@ -0,0 +1,4 @@
+package app.hotel.controllers;
+
+public class ChartController {
+}
