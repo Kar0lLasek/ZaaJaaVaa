b69416404ec2149a9f347f0df6a16ce398776e35
Mmarcopollo <szafranmarek97@gmail.com>
2020-04-17 22:56:42 +0200

    Add @Repository adnotation

--- JavaProject/server/src/main/java/app/database/repositories/UserRepository.java
+++ JavaProject/server/src/main/java/app/database/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package app.database.repositories;
 
 import app.database.entities.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<User,String> {
 }
